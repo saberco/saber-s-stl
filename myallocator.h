babf740ef8cb4dcd3eaf0a8d60768d7aefad0c8f
coco <1581075494@qq.com>
2022-03-01 14:25:14 +0800

    new update

--- myallocator.h
+++ myallocator.h
@@ -3,6 +3,7 @@
 #include<new>
 #include<iostream>
 
+//第一级配置器
 typedef void(*MALLOCALLOC)();           //将void (*)()   重命名成MALLOCALLOC
 template<int inst>
 class _MallocAllocTemplate
@@ -49,5 +50,47 @@ void* _MallocAllocTemplate<inst>::_OomMalloc(size_t n)
        return result;                              //申请成功时，则返回这块内存的地址
 }
 typedef _MallocAllocTemplate<0> malloc_alloc;
+
+//第二级配置器
+enum {_ALIGN = 8};                              //按照8的基准倍数进行内存操作
+enum {_MAXBYTES = 128};                         //自由链表最大的块大小为128
+enum {_NFREELISTS = 16};                        //自由链表的长度等于_MAXBYTES/_ALIGN
+template <bool threads, int inst>
+class _DefaultAllocTemplate
+{
+private:
+    union _Obj                                  //自由链表的结点类型
+    {
+        _Obj * _freeListLink;                   //指向自由链表的结点指针
+        char _clientData[1];
+    };
+private:
+    static char * _startFree;                   //内存池头指针
+    static char * _endFree;                     //内存池尾指针
+    static size_t _heapSize;                    //内存池已申请的内存大小
+    static _Obj * volatile _freeList[_NFREELISTS];  // 16个自由链表的头指针
+
+private:
+    //根据区块大小，决定使用第几号自由链表,n>0
+    static size_t _GetFreeListIndex(size_t bytes){
+        return (bytes + (size_t) _ALIGN - 1) / (size_t) _ALIGN - 1;
+    }
+
+    //将bytes上调至8的倍数
+    static size_t _GetRoundUp(size_t bytes){
+        return (bytes + (size_t) _ALIGN -1) & (~(_ALIGN-1));
+    }
+    static void * _Refill(size_t n);            //在自由链表中申请内存，n表示需要的内存大小
+    static void * _chunkAlloc(size_t, int & nobjs); //在内存池中申请nobjs个对象，每个对象size个大小
+
+public:
+    static void * Allocate(size_t n);       //n大于0
+    static void * DeAllocate(void *p, size_t n); 
+};
+//类外初始化静态成员变量和定义静态成员函数
+
+
+
+
 
 #endif
